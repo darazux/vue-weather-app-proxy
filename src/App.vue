15af504b0fb421e92d52f0e61e422392e3b38366
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-19 11:36:56 +0900

    * add Results component.

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,33 @@
 <template>
   <Title />
   <Form @submit-form="getWeather" />
+  <Results :results="results" />
 </template>
 
 <script setup>
 import axios from 'axios';
 import Title from './components/Title.vue';
 import Form from './components/Form.vue';
+import Results from './components/Results.vue';
+import { reactive } from 'vue';
+
+const results = reactive({
+  country: '',
+  cityName: '',
+  temperature: '',
+  conditionText: '',
+  icon: '',
+});
 
 const getWeather = (city) => {
   axios
     .get(`https://proxy-server-node.vercel.app/weather-data?${city}`)
-    .then((res) => console.log(res));
+    .then((res) => {
+      (results.country = res.data.location.country),
+        (results.cityName = res.data.location.name),
+        (results.temperature = res.data.current.temp_c),
+        (results.conditionText = res.data.current.condition.text),
+        (results.icon = res.data.current.condition.icon);
+    });
 };
 </script>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,1 +1,18 @@
 <!-- Results.vue -->
+
+<template>
+  <h2>気象データ</h2>
+  <div>{{ results.country }}</div>
+  <div>{{ results.cityName }}</div>
+  <div>{{ results.temperature }}</div>
+  <div v-if="results.icon">
+    <img :src="results.icon" alt="icon" />
+    <span>{{ results.conditionText }}</span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  results: Object,
+});
+</script>
