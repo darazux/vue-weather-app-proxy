dc1117882d64907b8fed6e3a85ef66dc75cda95a
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-19 11:08:05 +0900

    * add saving input form data.

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,7 +2,14 @@
 
 <template>
   <form>
-    <input type="text" placeholder="都市名を英語で入力" />
+    <input type="text" v-model="city" placeholder="都市名を英語で入力" />
+    {{ city }}
     <button type="submit">Get Weather</button>
   </form>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const city = ref('');
+</script>
